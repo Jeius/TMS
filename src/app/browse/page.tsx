6a61daaacd413d1b756fcb85c4c1442926705660
Julius Pahama <pahamajulius@gmail.com>
2024-09-18 16:22:31 +0800

    revise(browsePage): Revise the layout to adapt to screen size.

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 export default function Browse() {
     return (
-        <main className="grid grid-cols-5 grid-rows-4 p-5 gap-3">
-            <Card className="row-span-1 col-span-5">d</Card>
-            <Card className="row-span-4 col-span-1">s</Card>
-            <Table className="row-span-4 col-span-4" />
+        <main className="grid grid-rows-6 grid-cols-1 lg:grid-cols-5 lg:grid-rows-4 p-5 gap-3">
+            <Card className="lg:row-span-1 lg:col-span-5">d</Card>
+            <Card className="lg:row-span-4 lg:col-span-1">s</Card>
+            <Table className="row-span-4 lg:row-span-4 lg:col-span-4" />
         </main>
     )
 }
