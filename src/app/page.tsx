900417210793fca270b2cf08dfc8f44a9a538432
Julius Pahama <pahamajulius@gmail.com>
2024-10-02 20:45:19 +0800

    adjust(size): Adjust size of quick actions to adapt screen size.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import { Card, CardFooter, CardHeader } from '@/components/ui/card'
+import {
+    Card,
+    CardFooter,
+    CardHeader,
+} from '@/components/ui/card'
 
 import React from 'react'
 import QuickActions from './quick-actions'
@@ -8,30 +12,29 @@ import { Separator } from '@/components/ui/separator'
 const accountLinks = [
     { href: "#", label: "Profile" },
     { href: "#", label: "Account Management" },
-    { href: "#", label: "Settings" },
     { href: "#", label: "Logout" },
 ]
 
 export default function Home() {
     return (
-        <div className="max-w-none m-auto p-5">
+        <div className="flex justify-center max-w-none m-auto p-5">
             <div className="flex flex-col w-fit items-center space-y-16">
-                <Card className="w-full">
+                <Card className="w-full bg-gradient-to-b from-primary/40 to-primary/90">
                     <CardHeader className="space-y-1">
                         <h1 className="font-semibold text-lg">Welcome, [user]</h1>
-                        <span className="text-sm text-muted-foreground">[role]</span>
+                        <span className="text-sm">[role]</span>
                     </CardHeader>
                     <CardFooter className="flex flex-wrap justify-end space-x-1">
                         {accountLinks.map((link, index) => (
                             <div key={index} className='flex space-x-1 items-center'>
                                 <Link
                                     href={link.href}
-                                    className="text-sm font-semibold no-underline underline-offset-2 hover:text-muted-foreground hover:underline"
+                                    className="text-sm font-semibold no-underline underline-offset-2 hover:text-muted hover:underline"
                                 >
                                     {link.label}
                                 </Link>
 
-                                {index !== accountLinks.length - 1 && <Separator orientation="vertical" className="h-4" />}
+                                {index !== accountLinks.length - 1 && <Separator orientation="vertical" className="h-4 bg-foreground/50" />}
                             </div>
                         ))}
                     </CardFooter>
--- src/app/quick-actions.tsx
+++ src/app/quick-actions.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import {
     Carousel,
     CarouselContent,
@@ -6,12 +7,18 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
+import {
+    BookOpen,
+    CalendarFold,
+    FilePlus2,
+    Search,
+    Send,
+} from "lucide-react"
+import React from "react"
+import Link from "next/link"
 import Autoplay from "embla-carousel-autoplay"
 import { Card, CardContent } from '@/components/ui/card'
-import Link from "next/link"
-import { BookOpen, CalendarFold, FilePlus2, Search, Send, } from "lucide-react"
-import { useDeviceSize } from "@/hooks/use-device-size"
-import { screens } from "@/lib/utils"
+
 
 const actions = [
     { href: "#", label: "Submit Proposal", icon: <Send /> },
@@ -23,60 +30,60 @@ const actions = [
 ]
 
 export default function QuickActions() {
-    const deviceSize = useDeviceSize()
+    const plugin = React.useRef(
+        Autoplay({ delay: 2500, stopOnInteraction: true })
+    )
+
     return (
         <div className="flex flex-col items-start space-y-2 justify-center">
             <h2 className="font-semibold text-lg pl-2">Quick Actions</h2>
-            {deviceSize.width >= screens["md"]
-                ? <div id="grid-quick-actions" className="grid grid-cols-3 gap-4">
-                    {actions.map((action, index) => (
-                        <Link
-                            key={index}
-                            href={action.href}
-                        >
-                            <Card className="transition-all duration-150 hover:bg-accent ">
-                                <CardContent className="flex flex-col items-center justify-center space-y-2 p-6">
-                                    <div className="flex items-center justify-center size-10 p-2 bg-accent rounded-lg ">
-                                        {action.icon}
-                                    </div>
-                                    <span className="text-md font-semibold">{action.label}</span>
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    ))}
-                </div>
-                : <div className="px-14">
-                    <Carousel
-                        className="max-w-[300px] sm:max-w-sm md:max-w-md lg:max-w-none"
-                        plugins={[
-                            Autoplay({
-                                delay: 3000,
-                            }),
-                        ]}
+
+            <div id="grid-quick-actions" className="hidden md:grid grid-cols-3 gap-4 ">
+                {actions.map((action, index) => (
+                    <Link
+                        key={index}
+                        href={action.href}
                     >
-                        <CarouselContent className="-ml-1">
-                            {actions.map((action, index) => (
-                                <CarouselItem
-                                    key={index}
-                                    className="p-1 sm:basis-1/2"
-                                >
-                                    <Link href={action.href}>
-                                        <Card className="">
-                                            <CardContent className="flex flex-col items-center justify-center space-y-2 p-6">
-                                                <div className="flex items-center justify-center size-10 p-2 bg-accent rounded-lg ">
-                                                    {action.icon}
-                                                </div>
-                                                <span className="text-md font-semibold">{action.label}</span>
-                                            </CardContent>
-                                        </Card>
-                                    </Link>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </div>}
+                        <Card className="transition-all duration-150 hover:bg-accent">
+                            <CardContent className="flex min-w-60 flex-col items-center justify-center space-y-2 p-6">
+                                <div className="flex items-center justify-center size-10 p-2 bg-accent rounded-lg ">
+                                    {action.icon}
+                                </div>
+                                <span className="text-md font-semibold">{action.label}</span>
+                            </CardContent>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+
+            <div className="px-14 md:hidden">
+                <Carousel
+                    className="max-w-[300px] sm:max-w-md lg:max-w-none"
+                    plugins={[plugin.current]}
+                >
+                    <CarouselContent className="-ml-1">
+                        {actions.map((action, index) => (
+                            <CarouselItem
+                                key={index}
+                                className="p-1"
+                            >
+                                <Link href={action.href}>
+                                    <Card className="transition-all duration-150 hover:bg-accent">
+                                        <CardContent className="flex flex-col items-center justify-center space-y-2 p-6">
+                                            <div className="flex items-center justify-center size-10 p-2 bg-accent rounded-lg ">
+                                                {action.icon}
+                                            </div>
+                                            <span className="text-md font-semibold">{action.label}</span>
+                                        </CardContent>
+                                    </Card>
+                                </Link>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
         </div>
     )
 }
