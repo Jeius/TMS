fab64e542c5572654d5a84fa71df20fa6455bbb1
Julius Pahama <pahamajulius@gmail.com>
2024-10-16 19:41:11 +0800

    revise(motion): Apply motion to table components using framer-motion.

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,110 +1,114 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { MBody, MCaption, MTable, MTd, MTfoot, MTh, MThead, MTr } from "framer-motion-nextjs-elements"
+import { HTMLMotionProps } from "framer-motion"
 
 const Table = React.forwardRef<
   HTMLTableElement,
-  React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
-  <table
-    ref={ref}
-    className={cn("w-full caption-bottom text-sm", className)}
-    {...props}
-  />
-))
-Table.displayName = "Table"
+  HTMLMotionProps<"table"> & React.HTMLAttributes<HTMLTableElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "w-full caption-bottom text-sm";
+  return motion ? (
+    <MTable ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <table ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+Table.displayName = "Table";
 
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
-TableHeader.displayName = "TableHeader"
+  HTMLMotionProps<"thead"> & React.HTMLAttributes<HTMLTableSectionElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "[&_tr]:border-b";
+  return motion ? (
+    <MThead ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <thead ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableHeader.displayName = "TableHeader";
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  />
-))
-TableBody.displayName = "TableBody"
+  HTMLMotionProps<"tbody"> & React.HTMLAttributes<HTMLTableSectionElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "[&_tr:last-child]:border-0";
+  return motion ? (
+    <MBody ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <tbody ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableBody.displayName = "TableBody";
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tfoot
-    ref={ref}
-    className={cn(
-      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
-      className
-    )}
-    {...props}
-  />
-))
-TableFooter.displayName = "TableFooter"
-
-const TableRow = React.forwardRef<
-  HTMLTableRowElement,
-  React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted [&_td:last-child]:border-r-0 [&_th:last-child]:border-r-0",
-      className
-    )}
-    {...props}
-  />
-))
-TableRow.displayName = "TableRow"
-
-const TableHead = React.forwardRef<
-  HTMLTableCellElement,
-  React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      "h-10 px-2 border-r text-left align-middle font-semibold text-sm text-foreground",
-      className
-    )}
-    {...props}
-  />
-))
-TableHead.displayName = "TableHead"
+  HTMLMotionProps<"tfoot"> & React.HTMLAttributes<HTMLTableSectionElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0"
+  return motion ? (
+    <MTfoot ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <tfoot ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableFooter.displayName = "TableFooter";
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
-  React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <td
-    ref={ref}
-    className={cn(
-      "p-2 align-middle border-r",
-      className
-    )}
-    {...props}
-  />
-))
-TableCell.displayName = "TableCell"
+  HTMLMotionProps<"td"> & React.TdHTMLAttributes<HTMLTableCellElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "p-2 align-middle border-r";
+  return motion ? (
+    <MTd ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <td ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableCell.displayName = "TableCell";
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
-  React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
-  <caption
-    ref={ref}
-    className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-TableCaption.displayName = "TableCaption"
+  HTMLMotionProps<"caption"> & React.HTMLAttributes<HTMLTableCaptionElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "mt-4 text-sm text-muted-foreground";
+  return motion ? (
+    <MCaption ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <caption ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableCaption.displayName = "TableCaption";
+
+const TableRow = React.forwardRef<
+  HTMLTableRowElement,
+  HTMLMotionProps<"tr"> & React.HTMLAttributes<HTMLTableRowElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted [&_td:last-child]:border-r-0 [&_th:last-child]:border-r-0";
+
+  return motion ? (
+    <MTr ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <tr ref={ref} className={cn(defaultClassName, className)} {...props} />
+  );
+});
+TableRow.displayName = "TableRow";
+
+const TableHead = React.forwardRef<
+  HTMLTableCellElement,
+  HTMLMotionProps<"th"> & React.ThHTMLAttributes<HTMLTableCellElement> & { motion?: boolean }
+>(({ className, motion = false, ...props }, ref) => {
+  const defaultClassName = "h-10 px-2 border-r text-left align-middle font-semibold text-sm text-foreground"
+  return motion ? (
+    <MTh ref={ref} className={cn(defaultClassName, className)} {...props} />
+  ) : (
+    <th ref={ref} className={cn(defaultClassName, className)} {...props} />
+  )
+}
+)
+TableHead.displayName = "TableHead"
 
 export {
   Table,
