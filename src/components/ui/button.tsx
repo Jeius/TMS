31a5276b186e92bf2bc5a14ab578cdab43f28d6a
Julius Pahama <pahamajulius@gmail.com>
2024-10-16 21:44:53 +0800

    add(variant): Add gradient variant to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,13 +19,14 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        gradient: "bg-primary bg-gradient-to-b from-white/60 dark:bg-gradient-to-t dark:from-black/40 border hover:border-secondary hover:text-secondary",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
-      },
+      }
     },
     defaultVariants: {
       variant: "default",
@@ -36,7 +37,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
