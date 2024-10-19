d242d28c197a6f87c0cbbb567bcfee9327555362
Julius Pahama <pahamajulius@gmail.com>
2024-10-20 00:30:29 +0800

    create(type): Create a Thesis type.

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+
+export type Thesis = {
+    id: string
+    title: string
+    year: number
+    author: string
+    adviser: string
+    keywords: string
+    specialization: string
+    dateUploaded: string
+    department: string
+}
