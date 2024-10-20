1319312152b48a90491526288f60a5a10991c6cd
Julius Pahama <pahamajulius@gmail.com>
2024-10-20 10:55:49 +0800

    rename(QueryProvider): Rename from wrapper to provider.

---
+++ src/components/providers/query-provider.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export default function QueryProvider({ children }: { children?: React.ReactNode }) {
+  const client = new QueryClient({ defaultOptions: { queries: { staleTime: Infinity, refetchOnMount: false } } });
+  return (
+    <QueryClientProvider client={client} children={children} />
+  )
+}
