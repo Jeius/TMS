cf501bf9bd200da8a53b4ec052bf66422159a3e7
Julius Pahama <pahamajulius@gmail.com>
2024-10-20 21:15:56 +0800

    add(id): Add id to ThesesTable.

--- src/app/browse/_components/theses-table.tsx
+++ src/app/browse/_components/theses-table.tsx
@@ -4,6 +4,7 @@ import ThesesTableContent from '@/app/browse/_components/table-content'
 import { TableOptionsProps } from '@/app/browse/_components/table-header'
 import { Button } from '@/components/ui/button'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
 import { Thesis } from "@/utils/types"
 import { useQuery } from '@tanstack/react-query'
 import {
@@ -23,9 +24,9 @@ import { columns } from './table-columns'
 
 const TableOptions = dynamic(() => import('@/app/browse/_components/table-header')) as React.ComponentType<TableOptionsProps<any>>
 
-type TableProps = React.HtmlHTMLAttributes<HTMLElement> & { data: Thesis[] }
+type TableProps = React.HtmlHTMLAttributes<HTMLDivElement> & { data: Thesis[] }
 
-export default function ThesesTable({ data, ...props }: TableProps) {
+export default function ThesesTable({ data, className, ...props }: TableProps) {
     const [sorting, setSorting] = React.useState<SortingState>([]);
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
     const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({ author: false, year: false, department: false, dateUploaded: false });
@@ -48,20 +49,18 @@ export default function ThesesTable({ data, ...props }: TableProps) {
 
     return (
         <TooltipProvider>
-            <div {...props}>
-                <div className="relative">
-                    <div className="flex flex-col m-auto bg-card shadow border rounded-xl max-w-fit overflow-hidden">
-                        <TableOptions table={table} style={{ maxWidth: width }} />
-                        <ThesesTableContent table={table} />
-                        <div style={{ maxWidth: width }} className="w-full max-w-full overflow-hidden border-t-0 p-3 bg-card text-card-foreground">
-                            <Button size="lg"
-                                variant="gradient"
-                                className="p-2 sm:p-4 mx-auto flex space-x-2 font-sans"
-                            >
-                                <FileStackIcon aria-hidden="true" focusable="false" />
-                                <strong>Load more theses</strong>
-                            </Button>
-                        </div>
+            <div id="theses-table" className={cn("relative", className)} {...props}>
+                <div className="flex flex-col m-auto bg-card shadow border rounded-xl max-w-fit overflow-hidden">
+                    <TableOptions table={table} style={{ maxWidth: width }} />
+                    <ThesesTableContent table={table} />
+                    <div style={{ maxWidth: width }} className="w-full max-w-full overflow-hidden border-t-0 p-3 bg-card text-card-foreground">
+                        <Button size="lg"
+                            variant="gradient"
+                            className="p-2 sm:p-4 mx-auto flex space-x-2 font-sans"
+                        >
+                            <FileStackIcon aria-hidden="true" focusable="false" />
+                            <span className="font-bold">Load more theses</span>
+                        </Button>
                     </div>
                 </div>
             </div>
