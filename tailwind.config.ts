623f8fe43d865faf955b9c437b1fb0aa357ffbbd
Julius Pahama <pahamajulius@gmail.com>
2024-10-20 15:08:39 +0800

    create(uiComponent): Create a blurry blob for background.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,6 +62,23 @@ const config: Config = {
 			boxShadow: {
 				'right': '1px 0 5px rgba(0, 0, 0, 0.1)',
 			},
+			keyframes: {
+				"pop-blob": {
+					"0%": { transform: "scale(1)" },
+					"33%": { transform: "scale(1.2)" },
+					"66%": { transform: "scale(0.8)" },
+					"100%": { transform: "scale(1)" },
+				},
+				colors: {
+					filter: {
+						"blur-20": "blur(20px)",
+						"blur-25": "blur(25px)",
+					},
+				},
+			},
+			animation: {
+				"pop-blob": "pop-blob 5s infinite",
+			},
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
