2bc3f1faacca1f93644b297e4d9e80bfeb6ed22a
Julius Pahama <pahamajulius@gmail.com>
2024-10-21 21:46:21 +0800

    create(test-data): Create test data for the navigation links.

--- src/components/layout/navigation/navigation-items.tsx
+++ src/components/layout/navigation/navigation-items.tsx
@@ -1,39 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { primaryLinks, toolLinks, userLinks } from "@/utils/data/test/navigation-links";
 import { useQuery } from "@tanstack/react-query";
-import {
-    BookCopy,
-    BookOpenCheck,
-    CalendarDays,
-    ChartLine,
-    FilePlus2,
-    House,
-    Library,
-    MessageCircle,
-    Search
-} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-
-const primaryLinks = [
-    { href: '/', label: 'Home', icon: <House /> },
-    { href: '/browse', label: 'Browse', icon: <Search /> },
-];
-
-const userLinks = [
-    { href: '/my-library', label: 'My Library', icon: <Library /> },
-    { href: '/upload', label: 'Upload', icon: <FilePlus2 /> },
-    { href: '/borrow', label: 'Borrow', icon: <BookCopy /> },
-    { href: '/schedule', label: 'Schedule', icon: <CalendarDays /> },
-    { href: '/messages', label: 'Messages', icon: <MessageCircle /> },
-];
-
-const toolLinks = [
-    { href: '/analytics', label: 'Analytics', icon: <ChartLine /> },
-    { href: '/plagiarism-tool', label: 'Plagiarism Checker', icon: <BookOpenCheck /> },
-];
 
 
 export default function NavigationItems() {
---
+++ src/utils/data/test/navigation-links.tsx
@@ -0,0 +1,19 @@
+import { BookCopy, BookOpenCheck, CalendarDays, ChartLine, FilePlus2, House, Library, MessageCircle, Search } from "lucide-react";
+
+export const primaryLinks = [
+    { href: '/', label: 'Home', icon: <House /> },
+    { href: '/browse', label: 'Browse', icon: <Search /> },
+];
+
+export const userLinks = [
+    { href: '/my-library', label: 'My Library', icon: <Library /> },
+    { href: '/upload', label: 'Upload', icon: <FilePlus2 /> },
+    { href: '/borrow', label: 'Borrow', icon: <BookCopy /> },
+    { href: '/schedule', label: 'Schedule', icon: <CalendarDays /> },
+    { href: '/messages', label: 'Messages', icon: <MessageCircle /> },
+];
+
+export const toolLinks = [
+    { href: '/analytics', label: 'Analytics', icon: <ChartLine /> },
+    { href: '/plagiarism-tool', label: 'Plagiarism Checker', icon: <BookOpenCheck /> },
+];
