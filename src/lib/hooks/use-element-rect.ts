2ed36c3f89be88ed36d24e5bf2c00a5bf9ee9c53
Julius Pahama <pahamajulius@gmail.com>
2024-10-21 16:47:15 +0800

    create(hook): Create a hook that returns the rect of an element.

---
+++ src/lib/hooks/use-element-rect.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export function useElementRect(elementId: string) {
+    const [rect, setRect] = React.useState<DOMRect | null>(null);
+
+    const updateRect = React.useCallback(() => {
+        const element = document.getElementById(elementId);
+        if (element) {
+            setRect(element.getBoundingClientRect());
+        }
+    }, [elementId]);
+
+    React.useEffect(() => {
+        updateRect();
+
+        window.addEventListener("resize", updateRect);
+        return () => {
+            window.removeEventListener("resize", updateRect);
+        };
+    }, [updateRect]);
+
+    return rect;
+}
