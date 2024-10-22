44235d64ce8ea11a1ef8291168f5416e484c3269
Julius Pahama <pahamajulius@gmail.com>
2024-10-22 17:06:09 +0800

    wrap(button): Wrap the account links with button.

--- src/components/homepage/welcome-card.tsx
+++ src/components/homepage/welcome-card.tsx
@@ -1,11 +1,12 @@
 import { accountLinks } from '@/utils/data/test/navigation-links';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { Separator } from '../ui/separator';
 
 export default function WelcomeCard() {
     return (
-        <Card id="welcome-card" filter="glass" colorType="gradient" className="w-full bg-primary">
+        <Card id="welcome-card" variant="gradientGlass" className="w-full bg-primary/70 dark:bg-primary/75">
             <CardHeader className="space-y-1">
                 <CardTitle className="font-bold text-3xl text-secondary">
                     Welcome, [user]
@@ -14,15 +15,17 @@ export default function WelcomeCard() {
                     Role: [role]
                 </CardDescription>
             </CardHeader>
-            <CardFooter className="flex flex-wrap justify-end gap-2">
+            <CardFooter className="flex flex-wrap justify-end pt-4 gap-2">
                 {accountLinks.map((link, index) => (
-                    <div key={index} className='flex items-center space-x-2'>
-                        <Link
-                            href={link.href}
-                            className="flex items-center space-x-2 text-sm font-semibold no-underline underline-offset-2 hover:underline"
-                        >
-                            <span>{link.label}</span> {link.icon}
-                        </Link>
+                    <div key={index} className='flex items-center space-x-1'>
+                        <Button asChild variant="link" size="sm" className="size-min p-1 text-card-foreground">
+                            <Link
+                                href={link.href}
+                                className="flex items-center space-x-2 font-semibold"
+                            >
+                                <span>{link.label}</span> {link.icon}
+                            </Link>
+                        </Button>
 
                         {index !== accountLinks.length - 1 && (
                             <Separator orientation="vertical" className="h-4 bg-primary-foreground/60" />
