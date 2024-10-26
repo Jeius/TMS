6747a001cb29eb712a86e153744547619ea8b3e3
Julius Pahama <pahamajulius@gmail.com>
2024-10-26 18:48:03 +0800

    create(providers): Create a providers folder for the query provider and theme provider.

--- src/components/providers/query-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-export default function QueryProvider({ children }: { children?: React.ReactNode }) {
-  const client = new QueryClient({ defaultOptions: { queries: { staleTime: Infinity, refetchOnMount: false } } });
-  return (
-    <QueryClientProvider client={client}>{children}</QueryClientProvider>
-  )
-}
