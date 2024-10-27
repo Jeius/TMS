0a4093d8a904cef15438d77f5d638a55538c97c3
Julius Pahama <pahamajulius@gmail.com>
2024-10-28 00:52:05 +0800

    create(schema): Create schema for signin and signup forms.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import * as z from "zod";
 
 export type Thesis = {
     id: string
@@ -15,3 +15,25 @@ export type Thesis = {
 export const RemindersFormSchema = z.object({
     reminders: z.array(z.string()),
 })
+
+export type Message =
+    | { success: string }
+    | { error: string }
+    | { message: string };
+
+
+export const SignUpSchema = z
+    .object({
+        email: z.string().email("Invalid email address"),
+        password: z.string().min(8, "Password must be at least 8 characters long"),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        path: ["confirmPassword"],
+        message: "Passwords do not match",
+    });
+
+export const SignInSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+});
