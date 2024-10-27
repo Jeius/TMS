92794ab9b62154a2916f940aff0d32cd0f14f61e
Julius Pahama <pahamajulius@gmail.com>
2024-10-28 00:55:15 +0800

    revise(submit-button): Revise to handle form states from the parent.

--- src/components/animated/submit-button.tsx
+++ src/components/animated/submit-button.tsx
@@ -1,57 +1,40 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { CheckCircle2, CircleDashed, XCircleIcon } from "lucide-react";
-import { useState } from "react";
 
 import { cn } from "@/lib/utils";
-import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 
 const wait = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-type Status = "loading" | "success" | "failed";
+export type Status = "loading" | "success" | "failed";
 
 type SubmitButtonProps = React.ComponentPropsWithRef<typeof Button> & {
-    label?: string,
-    strokeColor?: string,
+    status?: Status;
+    isSubmitting?: boolean;
     fillColor?: string,
-}
+    strokeColor?: string,
+};
 
 export default function SubmitButton({
-    label = "Submit",
+    status,
+    isSubmitting,
     fillColor = "primary-foreground",
     strokeColor = "primary",
     className,
+    children,
     ...props
 }: SubmitButtonProps) {
-    const [status, setStatus] = useState<Status | undefined>();
-    const { pending } = useFormStatus();
-    const isEnabled = !status;
-
-    const changeStatus = async () => {
-        if (!isEnabled) return;
-
-        if (pending) {
-            setStatus("loading")
-        }
-        else {
-            setStatus("success");
-            await wait(1500);
-            setStatus(undefined);
-        }
-    };
-
     return (
         <Button
             variant="gradient"
             size="lg"
             type="submit"
             data-state={status}
-            onClick={changeStatus}
-            disabled={!isEnabled}
+            disabled={isSubmitting}
             className={cn(
-                "group relative h-10 min-w-40 overflow-hidden font-semibold duration-300",
+                "group relative h-10 w-full overflow-hidden font-semibold duration-300 disabled:opacity-100",
                 "data-[state=failed]:bg-destructive data-[state=failed]:hover:bg-destructive/80",
-                className
+                className,
             )}
             {...props}
         >
@@ -71,7 +54,7 @@ export default function SubmitButton({
                             animate={{ scale: 1 }}
                             transition={{ delay: 0.1, type: "spring" }}
                         >
-                            <CheckCircle2 className={`h-4 w-4 fill-${fillColor} stroke-destructive group-hover:stroke-destructive/80`} />
+                            <CheckCircle2 size={16} />
                         </motion.span>
                     )}
 
@@ -82,14 +65,14 @@ export default function SubmitButton({
                             animate={{ scale: 1 }}
                             transition={{ delay: 0.1, type: "spring" }}
                         >
-                            <XCircleIcon className={`h-4 w-4 fill-${fillColor} stroke-${strokeColor} group-hover:stroke-${strokeColor}/75`} />
+                            <XCircleIcon size={16} />
                         </motion.span>
                     )}
 
                     {status == "loading" ? (
                         <CircleDashed className={`h-4 w-4 animate-spin text-${fillColor}`} />
                     ) : (
-                        status ?? label
+                        status ?? children
                     )}
                 </motion.span>
             </AnimatePresence>
