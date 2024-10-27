c282875d72445be65a8ac76a0e62844f488f7fa0
Julius Pahama <pahamajulius@gmail.com>
2024-10-28 00:49:07 +0800

    rename(supabase-clients)

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
+export function supabaseServerClient() {
     const cookieStore = cookies()
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
