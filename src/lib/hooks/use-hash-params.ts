9dedddd8aa41ad939ba2651c7d52dd07ba979da2
Julius Pahama <pahamajulius@gmail.com>
2024-10-28 18:49:01 +0800

    create(useHashParams): Hook to read hash params.

---
+++ src/lib/hooks/use-hash-params.ts
@@ -0,0 +1,28 @@
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type HashParams = { [key: string]: string };
+
+export function useHashParams(): HashParams {
+    const router = useRouter();
+    const [hashParams, setHashParams] = useState<HashParams>({});
+
+    useEffect(() => {
+        const parseHashParams = () => {
+            const hash = window.location.hash;
+            const params = new URLSearchParams(hash.substring(1));
+            const result: HashParams = {};
+
+            params.forEach((value, key) => {
+                result[key] = value;
+            });
+
+            setHashParams(result);
+        };
+
+        parseHashParams();
+    }, [router]);
+
+    return hashParams;
+}
