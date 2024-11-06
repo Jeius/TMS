3961ebbb03ab0f2104934655be13cd3b46028639
Julius Pahama <pahamajulius@gmail.com>
2024-11-06 22:23:32 +0800

    create(hook): Create a hook to check for user in client side.

---
+++ src/lib/hooks/use-auth-listener.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { supabaseBrowserClient } from '../supabase/client';
+
+function useAuthListener() {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const supabase = supabaseBrowserClient();
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const { data: { user } } = await supabase.auth.getUser();
+            setIsSignedIn(!!user);
+            setIsMounted(true);
+        };
+        checkUser();
+
+        const { data: authListener } = supabase.auth.onAuthStateChange((event) => {
+            if (event === 'SIGNED_IN') setIsSignedIn(true);
+            if (event === 'SIGNED_OUT') setIsSignedIn(false);
+        });
+
+        return () => {
+            authListener?.subscription.unsubscribe();
+        };
+    }, [supabase]);
+
+    return { isSignedIn, isMounted };
+}
+
+export default useAuthListener;
