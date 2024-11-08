70311ce01ab06128cd8b8a7e3b8c26f4028d83f3
Julius Pahama <pahamajulius@gmail.com>
2024-11-08 20:27:51 +0800

    add(redirect): Add redirect option after sign up.

--- src/features/auth/login/server/actions.ts
+++ src/features/auth/login/server/actions.ts
@@ -3,6 +3,7 @@
 import { supabaseServerClient } from '@/lib/supabase/server';
 import { AuthActionResponse } from '@/lib/types';
 import { revalidatePath } from 'next/cache';
+import { headers } from 'next/headers';
 import * as z from 'zod';
 import { SignUpSchema } from '../lib/schema';
 
@@ -17,10 +18,14 @@ export async function signUpAction(data: z.infer<typeof SignUpSchema>): Promise<
 
     const { email, password } = result.data;
     const supabase = await supabaseServerClient();
+    const origin = (await headers()).get("origin");
 
     const { error: signUpError } = await supabase.auth.signUp({
         email,
         password,
+        options: {
+            emailRedirectTo: `${origin}/auth/confirm`,
+        },
     });
 
     if (signUpError) {
@@ -30,7 +35,7 @@ export async function signUpAction(data: z.infer<typeof SignUpSchema>): Promise<
         console.error(signUpError.code + ' ' + signUpError.message);
         return { error: 'Authentication error', details: signUpError.message };
     } else {
-        revalidatePath('/', 'layout');
+        revalidatePath('/');
         return {
             success: 'Signed up successfully',
             details: `Please verify your account! Confirmation link was sent to "${email}".`
